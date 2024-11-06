83c359d1db9e7058380c09448a5a9f39bb89bc8a
Jake van de Beek <j.vandebeek1@universityofgalway.ie>
2024-11-06 15:52:38 +0000

    Task 2

--- task2.py
+++ task2.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     test_losses = []
 
     # Epoch Loop
-    for epoch in range(1, 100):
+    for epoch in range(1, 5):
 
         # Start timer
         t = time.time_ns()
